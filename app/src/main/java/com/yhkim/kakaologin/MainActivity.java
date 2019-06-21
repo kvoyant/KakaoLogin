4a8f92a0502e08078d45d4b2d6cecf4c0b4e21b2
yhkim <kvoyant@gmail.com>
2019-06-21 13:45:46 +0900

    +앱실행시 사용자정보 가져오는 방식(버튼클릭방식이 아님)

--- app/src/main/java/com/yhkim/kakaologin/MainActivity.java
+++ app/src/main/java/com/yhkim/kakaologin/MainActivity.java
@@ -1,60 +1,109 @@
 package com.yhkim.kakaologin;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.kakao.auth.ErrorCode;
 import com.kakao.auth.ISessionCallback;
-import com.kakao.auth.KakaoSDK;
 import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
 import com.kakao.util.exception.KakaoException;
-import com.kakao.util.helper.log.Logger;
-
-public class MainActivity extends Activity {
-    private SessionCallback callback;
-    /**
-     * 로그인 버튼을 클릭 했을시 access token을 요청하도록 설정한다.
-     *
-     * @param savedInstanceState 기존 session 정보가 저장된 객체
-     */
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "";
+    SessionCallback callback;
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        KakaoSDK.init(new GlobalApplication.KakaoSDKAdapter());
+        // getHashKey();
 
+        setContentView(R.layout.activity_main);
         callback = new SessionCallback();
         Session.getCurrentSession().addCallback(callback);
-        Session.getCurrentSession().checkAndImplicitOpen();
+        requestMe();
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
-            return;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
+    public void requestMe() {
+        //유저의 정보를 받아오는 함수
+        UserManagement.getInstance().requestMe(new MeResponseCallback() {
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                Log.e(TAG, "error message=" + errorResult);
+//                super.onFailure(errorResult);
+            }
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                Log.d(TAG, "onSessionClosed1 =" + errorResult);
+            }
+            @Override
+            public void onNotSignedUp() {
+                //카카오톡 회원이 아닐시
+                Log.d(TAG, "onNotSignedUp ");
+            }
+            @Override
+            public void onSuccess(UserProfile result) {
+                Log.e("UserProfile", result.toString());
+                Log.e("UserProfile", result.getId() + "");
+            }
+        });
     }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Session.getCurrentSession().removeCallback(callback);
-    }
-    private class SessionCallback implements ISessionCallback {
+
+    class SessionCallback implements ISessionCallback {
+
         @Override
         public void onSessionOpened() {
-            redirectSignupActivity();
+
+            UserManagement.getInstance().requestMe(new MeResponseCallback() {
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+                    String message = "failed to get user info. msg=" + errorResult;
+
+                    ErrorCode result = ErrorCode.valueOf(errorResult.getErrorCode());
+                    if (result == ErrorCode.CLIENT_ERROR_CODE) {
+                        //에러로 인한 로그인 실패
+                        // finish();
+                    } else {
+                        //redirectMainActivity();
+                    }
+                }
+
+                @Override
+                public void onSessionClosed(ErrorResult errorResult) {
+                }
+
+                @Override
+                public void onNotSignedUp() {
+
+                }
+
+                @Override
+                public void onSuccess(UserProfile userProfile) {
+                    //로그인에 성공하면 로그인한 사용자의 일련번호, 닉네임, 이미지url등을 리턴합니다.
+                    //사용자 ID는 보안상의 문제로 제공하지 않고 일련번호는 제공합니다.
+
+                    // Log.e("UserProfile", userProfile.toString());
+                    // Log.e("UserProfile", userProfile.getId() + "");
+
+
+                    long number = userProfile.getId();
+
+
+                }
+            });
+
         }
+        // 세션 실패시
         @Override
         public void onSessionOpenFailed(KakaoException exception) {
-            if (exception != null) {
-                Logger.e(exception);
-            }
+
+
         }
     }
-    protected void redirectSignupActivity() {
-        final Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
+
 }
+
+
